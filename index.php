fbdef57f3d6d32242dab71795e8bec3cffd47036
Bleron Sylmetaj <84482589+bleronsy@users.noreply.github.com>
2023-05-21 14:45:26 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <!-- <nav>
+            <img src="./images/hamburger.png" alt="app-logo">
+            <h1>Fast Food App</h1>
+        </nav> -->
+        <div class="bigCnt bigCntHeader">
+            <div class="logo"></div> 
+           
+            <div class="miniCartTotal">0.00€</div>
+           <div class="alertProductAddToCart">
+           Shporta
+            </div>
+       </div>
+    </header>
+    <main>
+        <div class="pjesaKryesoreListaDheUshqimet">
+        <ul class="lista_e_ushqimeve">
+            <li><a href="./Oferta.php">Oferta</a></li>
+            <li><a href="./sandwich.php">Sandwich-a</a></li>
+            <li><a href="./hamburger.php">Hamburgera</a></li>
+            <li><a href="./pizza.php">Pizza</a></li>
+            <li><a href="./sallata.php">Sallata</a></li>
+            <li><a href="./pije.php">Pije</a></li>
+            <li><a href="./kycu.php">Kyçu</a></li>
+            <li><a href="./regjistrohu.php">Regjistrohu</a></li>
+        </ul>
+
+        <div class="gjithaUshqimet">
+            <p>ketu do vendosen gjitha ushqimet</p>
+        </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ regjistrohu.php
@@ -0,0 +1,43 @@
+<?php
+$host = 'localhost';
+$db_name = 'fooddelivery';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $Emri = $_POST['emri'] ?? '';
+    $Email = $_POST['email'] ?? '';
+    $Password = $_POST['password'] ?? '';
+    $Adresa = $_POST['adresa'] ?? '';
+    $NrTel = $_POST['nrtel'] ?? '';
+
+    $conn = new mysqli($host, 'root', '', $db_name);
+    if ($conn->connect_error) {
+        die('Connection Failed: ' . $conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO regjistrimi (Emri, Email, Password, Adresa, NrTel) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $Emri, $Email, $Password, $Adresa, $NrTel);
+        $stmt->execute();
+        echo "Registration successful...";
+        $stmt->close();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <form action="connect.php" method="post">
+            <input type="text" name="emri" placeholder="emri">
+            <input type="text" name="email" placeholder="email">
+            <input type="password" name="password" placeholder="password">
+            <input type="text" name="adresa" placeholder="adresa">
+            <input type="text" name="nrtel" placeholder="nr_tel">
+            <input type="submit">
+        </form>
+</body>
+</html>
